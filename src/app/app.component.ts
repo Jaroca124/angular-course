1694d53d4b24d653b6c3973c27690d0eff154d79
Jacob Caccamo <jacob.caccamo@gmail.com>
2022-04-05 14:13:15 -0400

    Adding Input Field

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-course';
+  data = {
+    title: 'angular-course'
+  }
+
+  onKeyupEvent(newTitle:string) {
+    this.data.title = newTitle;
+  }
 }
